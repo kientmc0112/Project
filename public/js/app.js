4cc0034bc7ccf4ca07d2b3fad52711b4328646a3
KienTran <kientmc0112@gmail.com>
2019-12-02 14:45:06 +0700

    add index

--- public/js/app.js
+++ public/js/app.js
@@ -121,10 +121,8 @@ $.ajaxSetup({
 });
 $(document).ready(function () {
   $('#course_id').on('click', function (event) {
-    event.preventDefault();
-    var id = $(this).attr('data-courseId');
-
-    var _this = $(this);
+    event.preventDefault(); // let id = $(this).attr('data-courseId');
+    // let _this = $(this);
 
     $.ajax({
       url: '/admin/user/' + id + '/export_subject',
@@ -301,6 +299,17 @@ $(document).ready(function () {
   \************************************************************************************************************************************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /var/www/html/Project/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/logout.js */"./resources/js/logout.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/courses.js */"./resources/js/courses.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/changeimg.js */"./resources/js/changeimg.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/option.js */"./resources/js/option.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/datatable.js */"./resources/js/datatable.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/addsubject.js */"./resources/js/addsubject.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/checkconfirm.js */"./resources/js/checkconfirm.js");
+__webpack_require__(/*! /var/www/html/Project/resources/js/ajax.js */"./resources/js/ajax.js");
+module.exports = __webpack_require__(/*! /var/www/html/Project/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
